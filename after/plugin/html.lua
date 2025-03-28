9b83ecda3cc93088ee7e9f11dd59ed8879f47036
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2025-03-28 09:43:17 -0300

    Format html

---
+++ after/plugin/html.lua
@@ -0,0 +1,23 @@
+vim.keymap.set("n", "<leader><leader>fhtml", function()
+  local buf_contents = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local input = table.concat(buf_contents, "\n")
+
+  local formatted = vim.fn.system({
+    "python3",
+    "-c",
+    [[
+import sys
+from bs4 import BeautifulSoup
+html = sys.stdin.read()
+soup = BeautifulSoup(html, 'html.parser')
+print(soup.prettify())
+    ]]
+  }, input)
+
+  if vim.v.shell_error == 0 then
+    local lines = vim.split(formatted, "\n", { plain = true })
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  else
+    vim.notify("HTML formatting failed", vim.log.levels.ERROR)
+  end
+end, { desc = "Format current buffer as HTML using BeautifulSoup" })
